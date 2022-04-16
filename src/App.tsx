9edfe031c6f1e3432e57eeffa6eb359a1f96b4fe
butiyj <buti0523@gmail.com>
2022-04-16 19:19:04 +0900

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Router } from "react-router-dom";
+import Header from "./Routes/Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import TV from "./Routes/TV";
+
 function App() {
-  return null;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/tv" element={<TV />}></Route>
+        <Route path="/search" element={<Search />}></Route>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
